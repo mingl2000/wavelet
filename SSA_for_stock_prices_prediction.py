436576574dc74627a6079d1309c183782d301efa
mingl2000 <mingl2000@gmail.com>
2023-02-12 14:09:57 -0800

    b1

--- SSA_for_stock_prices_prediction.py
+++ SSA_for_stock_prices_prediction.py
@@ -12,10 +12,65 @@ from ssa_core import ssa, ssa_predict, ssaview, inv_ssa, ssa_cutoff_order
 import matplotlib.pylab as plt
 import quandl
 import pandas as pd
+import sys
+import numpy as np
+
 import datetime
 from datetime import timedelta
 from dateutil import parser
+import pandas_datareader as pdr
+import pandas as pd
+import mplfinance as mpf
+import yfinance as yf
+from os.path import exists
 
+def GetYahooData(symbol, bars=500, interval='1d'):
+  #start=datetime.date.today()-datetime.timedelta(days=days)
+  #end=datetime.date.today()
+  if symbol=='SPX':
+    symbol='^GSPC'
+  #df=.gepdrt_data_yahoo(symbols=symbol,  start=start, end=end,interval=interval)
+  
+  #if interval.endswith('m') or interval.endswith('h'):
+  #  period='max'
+  
+  if interval.endswith('1m'):
+    period='7d'
+  elif  interval.endswith('m'):
+    period='60d'
+  elif  interval.endswith('h'):
+    period='730d'
+  else:
+    period='max'
+  
+  #elif interval.endswith('d'):
+    #period=str(days)+'d'
+  #  period='max'
+  #elif  interval.endswith('w'):
+  #  period=str(days)+'wk'
+  
+  dataFileName="data/"+symbol+'_' +period+'_'+ interval +".csv"
+  if interval.endswith(('d','D')) and datetime.datetime.now().hour>=13 and exists(dataFileName):
+    print('read yahoo data from cache')
+    df=pd.read_csv(dataFileName, header=0, index_col=0, encoding='utf-8', parse_dates=True)
+    #df.index=df["Date"]
+  else:
+    print('read yahoo data from web')
+    df = yf.download(tickers=symbol, period=period, interval=interval)
+    df.to_csv(dataFileName, index=True, date_format='%Y-%m-%d %H:%M:%S')
+  #dataFileName="data/"+symbol+".csv"
+  
+  #df = pd.read_csv(dataFileName,index_col=0,parse_dates=True)
+  #df.shape
+  df.dropna(inplace = True)
+  df =df [-bars:]
+  df.head(3)
+  df.tail(3)
+  df["id"]=np.arange(len(df))
+  #df["date1"]=df.index.astype(str)
+  #df["datefmt"]=df.index.strftime('%m/%d/%Y')
+  
+  return df
 
 #get_ipython().run_line_magic('matplotlib', 'inline')
 
@@ -38,23 +93,26 @@ def mae(f, t):
 # In[222]:
 
 
-instrument = 'MSFT'
-data = quandl.get('WIKI/%s' % instrument, start_date='2012-01-01', end_date='2017-02-01')
-closes = data['Adj. Close'].rename('close')
+symbol = 'MSFT'
+symbol = 'SPX'
+#data = quandl.get('WIKI/%s' % instrument, start_date='2017-01-01', end_date='2012-02-10')
 
+
+df=GetYahooData(symbol, bars=1000, interval='1d')
+closes = df['Adj Close'].rename('close')
 
 # We split series into train and test intervals and see how it looks on chart
 
 # In[223]:
 
 
-test_date = '2017-01-01'
+test_date = '2023-01-01'
 
 train_d = closes[:test_date]
 test_d = closes[test_date:]
 
 fig(16, 3); plt.plot(train_d, label='Train'); plt.plot(test_d, 'r', label='Test')
-plt.title('%s adjusted daily close prices' % instrument)
+plt.title('%s adjusted daily close prices' % symbol)
 plt.legend()
 
 
@@ -62,17 +120,17 @@ plt.legend()
 
 # In[224]:
 
+MAX_LAG_NUMBER = 120 # 4*30 = 1 quarter max
+MAX_LAG_NUMBER = 200 # 1 year?
 
 fig()
-ssaview(train_d.values, 120, [0,1,2,3])
+ssaview(train_d.values, MAX_LAG_NUMBER, [0,1,2,3])
 
 
 # We can plot residuals plot using following code
 
 # In[225]:
-
-
-pc, _, v = ssa(train_d.values, 120)
+pc, _, v = ssa(train_d.values, MAX_LAG_NUMBER)
 reconstructed = inv_ssa(pc, v, [0,1,2,3])
 noise = train_d.values - reconstructed
 plt.hist(noise, 50);
@@ -83,7 +141,6 @@ plt.hist(noise, 50);
 # In[226]:
 
 
-MAX_LAG_NUMBER = 120 # 4*30 = 1 quarter max
 n_co = ssa_cutoff_order(train_d.values, dim=MAX_LAG_NUMBER, show_plot=True)
 
 
@@ -111,7 +168,7 @@ orig = pd.DataFrame(test_d[:days_to_predict])
 plt.plot(orig, label='Test Data')
 plt.plot(f_ser, 'r-', marker='.', label='Forecast')
 plt.legend()
-plt.title('Forecasting %s for %d days, MAPE = %.2f%%' % (instrument, days_to_predict, mape(f_ser, orig)));
+plt.title('Forecasting %s for %d days, MAPE = %.2f%%' % (symbol, days_to_predict, mape(f_ser, orig)));
 plt.show()
 
 # In[ ]:
