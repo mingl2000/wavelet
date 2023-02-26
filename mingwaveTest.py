b3bf41ae787f8a865f1d4ba731de3b426d32304b
mingl2000 <mingl2000@gmail.com>
2023-02-25 16:27:01 -0800

    add ssa plt to volume

--- mingwaveTest.py
+++ mingwaveTest.py
@@ -27,7 +27,7 @@ from statsmodels.tsa.arima_model import ARIMA
 from statsmodels.tsa.stattools import adfuller
 from sklearn.ensemble import GradientBoostingRegressor as GBR
 from sklearn.metrics import explained_variance_score, mean_absolute_error, mean_squared_error, r2_score
-from model import WT, AR_MA, NonlinReg, ModelEvaluation
+from model import WT, AR_MA, NonlinReg, ModelEvaluation,WT_DWT
 import datetime
 import pandas_datareader as pdr
 import pandas as pd
@@ -36,8 +36,6 @@ import yfinance as yf
 from reliability.Other_functions import crosshairs
 from matplotlib.widgets import MultiCursor
 from os.path import exists
-from talib import ATR
-from scipy.ndimage import gaussian_filter1d
 # df = 'ohlc dataframe'
 
 # In[2]:
@@ -46,8 +44,6 @@ from scipy.ndimage import gaussian_filter1d
 from IPython.display import Image
 #get_ipython().run_line_magic('matplotlib', 'inline')
 
-def getATR(df, ATR_period):
-  return ATR(df["High"], df["Low"], df["Close"], ATR_period)[-1]
 
 # In[3]:
 def GetYahooData(symbol, bars=500, interval='1d'):
@@ -59,25 +55,23 @@ def GetYahooData(symbol, bars=500, interval='1d'):
   
   #if interval.endswith('m') or interval.endswith('h'):
   #  period='max'
-  
+  '''
   if interval.endswith('1m'):
     period='7d'
+  elif interval.endswith('d'):
+    period=str(days)+'d'
+  elif  interval.endswith('w'):
+    period=str(days)+'wk'
+  elif  interval.endswith('h'):
+    period=str(days) +'h'
   elif  interval.endswith('m'):
     period='60d'
-  elif  interval.endswith('h') or interval.endswith('d'):
-    period='730d'
   else:
     period='max'
-  
-  #elif interval.endswith('d'):
-    #period=str(days)+'d'
-  #  period='max'
-  #elif  interval.endswith('w'):
-  #  period=str(days)+'wk'
-  
+  '''
+  period='max'  
   dataFileName="data/"+symbol+'_' +period+'_'+ interval +".csv"
-  dataFileName1="data/"+symbol+'_' +'max'+'_'+ interval +".csv"
-  if interval.endswith(('d','D')) and datetime.datetime.now().hour>=13 and (exists(dataFileName) or exists(dataFileName1)):
+  if exists(dataFileName):
     print('read yahoo data from cache')
     df=pd.read_csv(dataFileName, header=0, index_col=0, encoding='utf-8', parse_dates=True)
     #df.index=df["Date"]
@@ -89,166 +83,25 @@ def GetYahooData(symbol, bars=500, interval='1d'):
   
   #df = pd.read_csv(dataFileName,index_col=0,parse_dates=True)
   #df.shape
-  df.dropna(inplace = True)
   df =df [-bars:]
   df.head(3)
   df.tail(3)
   df["id"]=np.arange(len(df))
   #df["date1"]=df.index.astype(str)
   #df["datefmt"]=df.index.strftime('%m/%d/%Y')
-  
   return df
-
-#SSA_compare
-from pyts.decomposition import SingularSpectrumAnalysis
-from numpy import pi
-
-def plot_ssa_compare(df, symbol, window_sizes):
-  if window_sizes ==None:
-    window_sizes=[5, 10,15,20, 25,30]
-  if isinstance(window_sizes, str):
-    window_sizes_temp=[]
-    for s in window_sizes.split(','):
-      window_sizes_temp.append(int(s))
-    window_sizes=window_sizes_temp
-
-  #data = quandl.get('WIKI/%s' % instrument, start_date='2017-01-01', end_date='2012-02-10')
-
-
-  #df=GetYahooData(symbol, bars=bars, interval='1d')
-  #closes = df['Adj Close'].rename('close')
-  closes = df['Close']
-  '''
-  N=20
-  t = np.arange(0,N)
-
-  trend = 0.001 * (t - 100)**2
-
-  p1, p2 = 20, 30
-
-  periodic1 = 2 * np.sin(2*pi*t/p1)
-  periodic2 = 0.75 * np.sin(2*pi*t/p2)
-
-  np.random.seed(123) # So we generate the same noisy time series every time.
-  noise = 2 * (np.random.rand(N) - 0.5)
-  F = trend + periodic1 + periodic2 + noise
-
-  # Parameters
-  n_samples, n_timestamps = 1000, 10
-
-  # Toy dataset
-  rng = np.random.RandomState(41)
-  X = rng.randn(n_samples, n_timestamps)
-  '''
-  X=[]
-  X.append(closes)
-
-  # We decompose the time series into three subseries
-  #window_size = 2
-
-  #groups = [np.arange(i, i + 5) for i in range(0, 11, 5)]
-
-  # Singular Spectrum Analysis
-  #plt.figure(figsize=(16, 6))
-  #plt.plot(closes.to_numpy(), 'o-', label='Original')
-
-  import mplfinance as mpf
-  figsize=(26,13)
-  mc = mpf.make_marketcolors(
-                            volume='lightgray'
-                            )
-
-                            
-  s  = mpf.make_mpf_style(marketcolors=mc, gridaxis='both')
-  apdict = []
-  for window_size in window_sizes:
-    #window_size=i
-    ssa = SingularSpectrumAnalysis(window_size=window_size, groups=None)
-    X_ssa = ssa.fit_transform(X)
-    newcol='ssa_'+str(window_size)
-    df[newcol]=X_ssa[0]
-
-    apdict.append(mpf.make_addplot(df[newcol], secondary_y=False))
-
-    if window_size==window_sizes[int(len(window_sizes)/2)]:
-      newcol_diff='ssa_diff_'+str(window_size)
-      df[newcol_diff]=df['Close']-X_ssa[0]
-      #apdict.append(mpf.make_addplot(df['Close']-X_ssa[0], panel=1,secondary_y=False,ylabel='diff'))
-      #apdict.append(mpf.make_addplot(df['High']-X_ssa[0], panel=1, secondary_y=False))
-      #apdict.append(mpf.make_addplot(df['Low']-X_ssa[0], panel=1, secondary_y=False))
-
-      def plot_ssa_diff_std(colname, X_ssa):
-        apdict.append(mpf.make_addplot(df[colname]-X_ssa[0], panel=1,secondary_y=False,ylabel='diff'))
-        #apdict.append(mpf.make_addplot(df[newcol_diff], panel=1,ylabel=newcol_diff, secondary_y=False))
-
-        newcol_diff_1_std_ub='ssa_diff_1std_up_'+colname +' '+str(window_size) 
-        newcol_diff_1_std_lb='ssa_diff_1std_low_'+colname +' '+str(window_size)
-        stderr=np.std((df[colname]-X_ssa[0]).to_numpy())
-        print('stderr', stderr)
-        df[newcol_diff_1_std_ub]=stderr
-        df[newcol_diff_1_std_lb]=-stderr
-        apdict.append(mpf.make_addplot(df[newcol_diff_1_std_ub], panel=1,width=2,color='b', secondary_y=False))
-        apdict.append(mpf.make_addplot(df[newcol_diff_1_std_lb], panel=1,width=2, color='b', secondary_y=False))
-
-        newcol_diff_2_std_ub='ssa_diff_2std_up_'+str(window_size)
-        newcol_diff_2_std_lb='ssa_diff_2std_low_'+str(window_size)
-        df[newcol_diff_2_std_ub]=2*stderr
-        df[newcol_diff_2_std_lb]=-2*stderr
-        apdict.append(mpf.make_addplot(df[newcol_diff_2_std_ub], panel=1,width=3, color='r', secondary_y=False))
-        apdict.append(mpf.make_addplot(df[newcol_diff_2_std_lb], panel=1,width=3, color='r', secondary_y=False))
-      
-      plot_ssa_diff_std('Close', X_ssa)
-      plot_ssa_diff_std('High', X_ssa)
-      plot_ssa_diff_std('Low', X_ssa)
-
-      print(window_size, X_ssa[0][-1], len(X_ssa[0]) )
-
-
-
-
-    #print(X_ssa)
-    # Show the results for the first time series and its subseries
-
-    #ax1 = plt.subplot(211)
-    #plt.plot(X_ssa[0],  label=('X_ssa[0] window=' +str(window_size)))
-    
-    #plt.legend(loc='best', fontsize=14)
-
-  '''
-  plt.suptitle('Singular Spectrum Analysis', fontsize=20)
-
-  plt.tight_layout()
-  plt.subplots_adjust(top=0.88)
-  plt.show()
-  '''
-
-  fig1,ax1=mpf.plot(df,type='candle',volume=False,volume_panel=2,addplot=apdict, figsize=figsize,tight_layout=True,style=s,returnfig=True,block=False, title=ticker,panel_ratios=(1,2))
-  V=[]
-  V.append(df['Volume'])
-  ssav = SingularSpectrumAnalysis(window_size=window_size, groups=None)
-  V_ssa = ssav.fit_transform(V)
-  df['V_ssa']=V_ssa[0]
-  apdict.append(mpf.make_addplot(df['V_ssa'], secondary_y=False, panel=2))
-
-  fig1,ax1=mpf.plot(df,type='candle',volume=True,volume_panel=2,addplot=apdict, figsize=figsize,tight_layout=True,style=s,returnfig=True,block=False, title=ticker)
-
-
-  # The first subseries consists of the trend of the original time series.
-  # The second and third subseries consist of noise.
 
 def multi_plot_wt(df, wavlet_close, wavlet_high, wavlet_low):
     plt.margins(0.1)
     fig, ax =  plt.subplots(len(wavlet_close), 1, figsize=figsize)
     for i in range(len(wf_close)):
         if i == 0:
-            #ax[i].set_yscale("log");
             ax[i].plot(df["id"],df['Close'], label = 'Close',color='lightgray')
             ax[i].plot(df["id"],wavlet_close[i], label = 'Close cA[%.0f]'%(len(wavlet_close)-i-1),linewidth=2)
             ax[i].plot(df["id"],wavlet_high[i], label = 'High cA[%.0f]'%(len(wavlet_close)-i-1))
             ax[i].plot(df["id"],wavlet_low[i], label = 'Low cA[%.0f]'%(len(wavlet_close)-i-1))
             
             ax[i].legend(loc = 'best')
-            
         else:
             ax[i].plot(df["id"],wavlet_close[i], label = 'close-cD[%.0f]'%(len(wavlet_close)-i))
             ax[i].plot(df["id"],wavlet_high[i], label = 'high-cD[%.0f]'%(len(wavlet_close)-i))
@@ -278,8 +131,8 @@ def plot_wt(df, colname, wavelet):
 
 
 def printwavelet(daysprint, df, wf_close, wf_high, wf_low, wf_vol):
-  print('day                  close         close1       high             high1         low               low1              volume                  volume1                   Gaussian Filter3                   Gaussian Filter5')
-  fmt="{0:18}{1:8.2f} * {2:8.2f} {3:4} {4:8.2f} {5:4} * {6:8.2f} {7:4} {8:8.2f} {9:4} * {10:8.2f} {11:4} {12:8.2f} {13:4} * {14:18,.0f} {15:4} {16:18,.0f} {17:4} {18:18,.2f} {19:18,.2f}"
+  print('day                  close         close1       high             high1         low               low1              volume                  volume1')
+  fmt="{0:18}{1:8.2f} * {2:8.2f} {3:4} {4:8.2f} {5:4} * {6:8.2f} {7:4} {8:8.2f} {9:4} * {10:8.2f} {11:4} {12:8.2f} {13:4} * {14:18,.0f} {15:4} {16:18,.0f} {17:4}"
   for i in range(daysprint,-1,-1):  
     if wf_close[0][-i-1]>wf_close[0][-i-2]:
       closedir='UP'
@@ -317,20 +170,15 @@ def printwavelet(daysprint, df, wf_close, wf_high, wf_low, wf_vol):
     else:
       vol1dir='DOWN'
     
-    print(fmt.format(df.index[-i-1].strftime("%m/%d/%Y %H:%M"), df['Close'][-i-1], wf_close[0][-i-1],closedir,wf_close[1][-i-1],close1dir,wf_high[0][-i-1],highdir,wf_high[1][-i-1],high1dir,wf_low[0][-i-1],lowdir,wf_low[1][-i-1],low1dir,wf_vol[0][-i-1],voldir,wf_vol[1][-i-1],vol1dir, gf3[i], gf5[i]))
+    print(fmt.format(df.index[-i-1].strftime("%m/%d/%Y %H:%M"), df['Close'][-i-1], wf_close[0][-i-1],closedir,wf_close[1][-i-1],close1dir,wf_high[0][-i-1],highdir,wf_high[1][-i-1],high1dir,wf_low[0][-i-1],lowdir,wf_low[1][-i-1],low1dir,wf_vol[0][-i-1],voldir,wf_vol[1][-i-1],vol1dir))
 
 
-brick_size=0.1 # real brick_size will be brick_size*ATR(14)
 drawchart=True
 historylen=512
-interval='1wk'
+interval='1d'
 daysprint=89
 usecache=True
 daystoplot=512
-if len(sys.argv) <1:
-  print("arguments are : Symbol historylen interval drawchart daysprint brick_size_in_ATR")
-  print("interval can be 1m, 5m, 15m, 30m, 1h, 1d, 1wk, 1mo, 3mo")
-  print("python .\mingwave3.py QQQ 256 1d True 20 True 128 0l5")
 if len(sys.argv) <2:
   ticker='QQQ'
 if len(sys.argv) >=2:
@@ -347,46 +195,35 @@ if len(sys.argv) >=7:
   usecache=sys.argv[6].lower()=='true'
 if len(sys.argv) >=8:
   daystoplot=int(sys.argv[7])
-if len(sys.argv) >=9:
-  brick_size=float(sys.argv[8])
+
+
+  print("arguments are : Symbol historylen interval drawchart daysprint")
+  print("interval can be 1m, 5m, 15m, 30m, 1h, 1d, 1wk, 1mo, 3mo")
   #exit()
 
 
 #ticker="SPX"
 df= GetYahooData(ticker,historylen,interval)
-x= df["Close"].to_numpy() 
-gf2 = gaussian_filter1d(x, 2)
-gf3 = gaussian_filter1d(x, 3)
-gf5 = gaussian_filter1d(x, 5)
-gf8 = gaussian_filter1d(x, 8)
-gf13 = gaussian_filter1d(x, 13)
-gf21 = gaussian_filter1d(x, 21)
-df["gf2"]=gf2
-df["gf3"]=gf3
-df["gf5"]=gf5
-df["gf8"]=gf8
-df["gf13"]=gf13
-df["gf21"]=gf21
-brick_size=brick_size*getATR(df, 14)
-
 # import data
 #df = pd.read_csv(filename, header=0, index_col=0, encoding='utf-8')
 df.head()
 
+
 wf_close=WT(df['Close'], plot=False)
-wf_close_lt=WT(df['Close'], wavefunc='db4', lv=5, m=1, n=5,plot=False)
 wf_high=WT(df['High'], plot=False)
 wf_low=WT(df['Low'], plot=False)
 wf_vol=WT(df['Volume'], plot=False)
 
 df["coeff_close"] = wf_close[0]
-df["coeff_close_lt"] = wf_close_lt[0]
 df["coeff_high"]= wf_high[0]
 df["coeff_low"]= wf_low[0]
 df["coeff_vol"] =wf_vol[0]
 
 df["coeff_close_01"] = wf_close[0]+wf_close[1]
 df["coeff_vol_01"] = wf_vol[0]+wf_vol[1]
+
+df["dwt_close_cA"],df["dwt_close_c"] =WT_DWT(df['Close'], plot=False)
+
 
 printwavelet(daysprint, df,wf_close, wf_high, wf_low, wf_vol)
 '''
@@ -446,14 +283,8 @@ for i in range(len(wf_close)):
 mc = mpf.make_marketcolors(
                            volume='lightgray'
                            )
-
-                          
-s  = mpf.make_mpf_style(marketcolors=mc, gridaxis='both')
-apdict = [
-#        mpf.make_addplot(df["gf3"], width=3, color='r',linestyle='dashdot'),
-#        mpf.make_addplot(df["gf5"], width=5, color='y',linestyle='dashdot'),
-        mpf.make_addplot(df['coeff_close']),
-        mpf.make_addplot(df['coeff_close_lt'], width=3, color='b'),
+s  = mpf.make_mpf_style(marketcolors=mc)
+apdict = [mpf.make_addplot(df['coeff_close']),
         mpf.make_addplot(df['coeff_high']),
         mpf.make_addplot(df['coeff_low']),
         mpf.make_addplot(df['coeff_close_01']),
@@ -463,39 +294,31 @@ apdict = [
         #mpf.make_addplot(wf_vol[1],panel=2,ylabel='wf_vol[1]',y_on_right=False),
         mpf.make_addplot((df['coeff_vol']),panel=1,color='r'),
         mpf.make_addplot((df['coeff_vol_01']),panel=1,color='g')]
-fig1,ax1=mpf.plot(df,type='candle',volume=True,addplot=apdict, figsize=figsize,tight_layout=True,style=s,returnfig=True,block=False,title=ticker)
+fig1,ax1=mpf.plot(df,type='candle',volume=True,addplot=apdict, figsize=figsize,tight_layout=True,style=s,returnfig=True,block=False)
 
 
-fig2,ax2=mpf.plot(df,type='candle',volume=True,addplot=apdict, figsize=figsize,tight_layout=True, panel_ratios=(1,1),style=s,returnfig=True,block=False,title=ticker)
+fig2,ax2=mpf.plot(df,type='candle',volume=True,addplot=apdict, figsize=figsize,tight_layout=True, panel_ratios=(1,1),style=s,returnfig=True,block=False)
 
-apdict = [
-        mpf.make_addplot(df["gf3"], width=3, color='r',linestyle='dashdot'),
-        mpf.make_addplot(df["gf5"], width=5, color='y',linestyle='dashdot'),
-        mpf.make_addplot(df['coeff_close']),
-        mpf.make_addplot(df['coeff_close_lt'], width=3, color='b'),
-        mpf.make_addplot(df['coeff_high']),
-        mpf.make_addplot(df['coeff_low']),
+apdict = [mpf.make_addplot(df['coeff_close'],width=4, color='r'),
+        #mpf.make_addplot(df['coeff_high']),
+        #mpf.make_addplot(df['coeff_low']),
         mpf.make_addplot(df['coeff_close_01']),
+        mpf.make_addplot(df['dwt_close_cA'],width=4, color='b'),
         mpf.make_addplot(wf_low[1],panel=1),
         mpf.make_addplot(wf_close[1],panel=1,ylabel='wf_close[1]',y_on_right=True),
         mpf.make_addplot(wf_high[1],panel=1),
+        
         #mpf.make_addplot(wf_vol[1],panel=2,ylabel='wf_vol[1]',y_on_right=False),
         #mpf.make_addplot((df['coeff_vol']),panel=1,color='g'),
         #mpf.make_addplot((df['coeff_vol_01']),panel=1,color='g')
         ]
 
-fig3,ax3=mpf.plot(df,type='candle',volume=False,addplot=apdict, figsize=figsize,tight_layout=True,returnfig=True,block=False,title=ticker)
-fig6,ax6=mpf.plot(df,type='renko',volume=False, figsize=figsize,tight_layout=True,returnfig=True,block=False, renko_params=dict(brick_size=brick_size),title=ticker)
+fig3,ax3=mpf.plot(df,type='candle',volume=False,addplot=apdict, figsize=figsize,tight_layout=True,returnfig=True,block=False)
 (fig5, ax5)=plot_wt(df, 'Volume', wf_vol)
 (fig4, ax4)=multi_plot_wt(df, wf_close, wf_high,wf_low)
-
-#plot_ssa_compare
-window_sizes='5, 10,15,20, 25,30'
-plot_ssa_compare(df, ticker, window_sizes)
 
 #cursor = MultiCursor(None, tuple(ax1)+tuple(ax2)+tuple(ax3)+tuple(ax4)+tuple(ax5), color='r',lw=0.5, horizOn=True, vertOn=True)
 plt.show()
 #crosshairs(xlabel='t',ylabel='F')
-#ssa_compare
 
 
