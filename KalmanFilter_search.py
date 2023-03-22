0866526dfeb8ed1bcce1eded4c43382428ae800a
mingl2000 <mingl2000@gmail.com>
2023-03-22 14:53:47 -0700

    KF search update

---
+++ KalmanFilter_search.py
@@ -0,0 +1,106 @@
+import sys
+from termcolor import colored
+from pykalman import KalmanFilter
+from YahooData import *
+import sys
+import pandas as pd
+import warnings
+
+warnings.filterwarnings('ignore')
+#from kalmanfilter3 import *
+
+def DoKalmanFilter(arr):
+    kf = KalmanFilter(
+      initial_state_mean=arr[0],
+      initial_state_covariance=1,
+      observation_covariance=1,
+      transition_covariance=0.01
+    )
+    kf_means, _ = kf.filter(arr)
+    kf_max=kf_means[-1][0]
+    kf_min=kf_means[-1][0]
+    #find max
+    for i in range(len(kf_means)-2,0,-1):
+        if kf_means[-2][0]>kf_means[-1][0]: # going down
+            if kf_means[i][0]>kf_means[i+1][0]:
+                kf_max=kf_means[i][0]
+            else:
+               break
+        if kf_means[-2][0]<kf_means[-1][0]: # going up
+           break
+    #find min
+    for i in range(len(kf_means)-2,0,-1):
+        if kf_means[-2][0]<kf_means[-1][0]: # going up
+            if kf_means[i][0]<kf_means[i+1][0]:
+                kf_min=kf_means[i][0]
+            else:
+               break
+        if kf_means[-2][0]>kf_means[-1][0]: # going down
+           break
+        
+    return (kf_means, kf_min, kf_max)
+
+def doKalmarFilerSearch(symbols):
+
+    kf_columns={'ticker':[], 
+            'Close':[],             
+            'High':[], 
+            'Low':[],
+            'Open':[],
+            'Volume':[], 
+            'kf':[],
+            'kfmin':[],
+            'kfmax':[],
+            'kfslope':[],
+            'abovekfmin':[],
+            'belowkfmax':[],
+            'closemin':[],
+            'closemax':[],
+            'closeslope':[],
+            'aboveclosemin':[],
+            'belowclosemax':[]
+            }
+    kf_df=pd.DataFrame(kf_columns)
+    kf_df.set_index('ticker')
+
+    for symbol in symbols.split(','):
+        df=GetYahooData_v2(symbol, bars=500, interval='1d')
+        (kf_means, kf_min, kf_max)=DoKalmanFilter(df['Close'].to_numpy())
+        kf_df.loc[symbol] = [symbol,
+                            df['Close'][-1],
+                            df['High'][-1],
+                            df['Low'][-1],
+                            df['Open'][-1],
+                            df['Volume'][-1],
+                            kf_means[-1][0],
+                            kf_min,
+                            kf_max,
+                            kf_means[-1][0]/kf_means[-2][0]*100-100,
+                            kf_means[-1][0]-kf_min,
+                            kf_max-kf_means[-1][0],
+                            None,
+                            None,
+                            df['Close'][-1]/df['Close'][-2]*100-100,
+                            None,
+                            None
+                            ]
+    return kf_df
+
+
+if len(sys.argv) <2:
+  symbols='QQQ,SPX'
+if len(sys.argv) >=2:
+  symbols=sys.argv[1]
+
+df=doKalmarFilerSearch(symbols)
+for i in range(len(df)):
+   print(df.index[i],'kf=', df['kf'][i],'kfmin=',df['kfmin'][i], 'dfmax=', df['kfmax'][i],'kfslope=',df['kfslope'][i])
+df=df.sort_values(by=['kfslope','belowkfmax','abovekfmin'])
+
+fmt="KarlmanFilter_search_{0}.xlsx"
+filename=fmt.format(datetime.datetime.now().strftime("%Y_%m_%d_%H_%M_%S"))
+df.to_excel(filename)
+
+from termcolor import colored
+print('Check output file:' +colored(filename,'red'))
+
---
+++ kalmanfilter_search_small.bat
@@ -0,0 +1,1 @@
+python .\KalmanFilter_search.py QQQ,SPX,000001.ss,399001.sz,002049.sz,002230.sz,002466.sz,300750.sz,600745.ss,603986.ss,688366.ss,688088.ss,300223.sz,002460.sz,002129.sz,601318.ss
